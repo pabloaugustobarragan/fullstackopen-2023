0defc79caba4e103d0d0d4cb665a90c147407a61
Pablo Barragán <pabloaugustobarragan@gmail.com>
2023-11-21 19:15:07 -0300

    Resuelto part1-5

--- part1/src/index.js
+++ part1/src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 const Header = ({ course }) => {
-  return <h1>{course}</h1>
+  return <h1>{course.name}</h1>
 }
 
 const Content = ({ data }) => {
@@ -25,23 +25,29 @@ const Total = ({ exercises }) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-
-  /*const part1 = { name: 'Fundamentals of React', exercises: 10 }
-  const part2 = { name: 'Using props to pass data', exercises: 7 }
-  const part3 = { name: 'State of a component', exercises: 14 }*/
-
-  const parts = [
-    { name: 'Fundamentals of React', exercises: 10 },
-    { name: 'Using props to pass data', exercises: 7 },
-    { name: 'State of a component', exercises: 14 }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
       <Header course={course} />
-      <Content data={parts} />
-      <Total exercises={parts} />
+      <Content data={course.parts} />
+      <Total exercises={course.parts} />
 
     </div>
   )
