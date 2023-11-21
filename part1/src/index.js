a449166024c039061347e52c2bca1fcffe59013a
Pablo Barragán <pabloaugustobarragan@gmail.com>
2023-11-21 19:12:42 -0300

    Resuelto part1-4

--- part1/src/index.js
+++ part1/src/index.js
@@ -20,24 +20,28 @@ const Part = ({ part, exercises }) => {
 }
 
 const Total = ({ exercises }) => {
-  console.log(exercises);
-  const total = exercises.reduce((sum, a) => sum + a, 0);
-  console.log(total);
+  const total = exercises.reduce((sum, a) => sum + a.exercises, 0);
   return <p>Number of exercises {total}</p>;
 }
 
 const App = () => {
   const course = 'Half Stack application development'
 
-  const part1 = { name: 'Fundamentals of React', exercises: 10 }
+  /*const part1 = { name: 'Fundamentals of React', exercises: 10 }
   const part2 = { name: 'Using props to pass data', exercises: 7 }
-  const part3 = { name: 'State of a component', exercises: 14 }
+  const part3 = { name: 'State of a component', exercises: 14 }*/
+
+  const parts = [
+    { name: 'Fundamentals of React', exercises: 10 },
+    { name: 'Using props to pass data', exercises: 7 },
+    { name: 'State of a component', exercises: 14 }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content data={[part1, part2, part3]} />
-      <Total exercises={[part1.exercises, part2.exercises, part3.exercises]} />
+      <Content data={parts} />
+      <Total exercises={parts} />
 
     </div>
   )
